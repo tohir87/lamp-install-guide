e876a3492abf75fd15f0a644fefa475379700f2c
Tohir Omoloye <omoloye.tohir@yahoo.co.uk>
2020-02-10 14:05:39 +0000

    Creating buckets

---
+++ boto3/create_bucket.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+import boto3
+s3 = boto3.resource("s3")
+for bucket_name in sys.argv[1:] :
+	try:
+		response = s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'eu-west-1'})
+		print (response)
+	except Exception as error:
+		print (error)
